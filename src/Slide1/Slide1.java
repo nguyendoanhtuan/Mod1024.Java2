11460fd443b99e96389d21c5bf8b8b355903c4b4
PandaG <pandago210222@gmail.com>
2026-03-25 11:34:26 +0700

    upload LAB 1

---
+++ src/Slide1/Slide1.java
@@ -0,0 +1,11 @@
+package Slide1;
+
+public class Slide1 {
+	public static void main(String[] args) {
+		Xe xe = new Xe();
+		System.out.println("36 PS47847 NguyenDoAnhTuan");
+		xe.setNamSanXuat(2026);
+		xe.sethangXe("Toyota");
+		System.out.println(xe.toString());
+	}
+}
--- src/Slide1/Xe.java
+++ src/Slide1/Xe.java
@@ -2,4 +2,23 @@ package Slide1;
 
 public class Xe {
 	private String hangXe ;
+	private int namSanXuat ; 
+	
+	public String gethangXe() {
+		return this.hangXe ;
+	};
+	public void sethangXe(String hangXe) {
+		this.hangXe = hangXe;
+	}
+	public int getNamSanXuat() {
+		return namSanXuat;
+	}
+	public void setNamSanXuat(int namSanXuat) {	
+		this.namSanXuat = namSanXuat;
+	}
+	@Override
+	public String toString() {
+		return "Xe [hangXe=" + hangXe + ", namSanXuat=" + namSanXuat + "]";
+	}
+	
 }
